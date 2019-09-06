756ced4b9e097a86a3535e5ce53545c20f2d20a5
Erick Rivas <erick_rivas@outlook.com>
2019-09-06 02:07:34 -0500

    Catch builder errors

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ exports.build = function (input, platform, output) {
   const exec = require("child_process").execSync;
   let dir = __dirname.replace(/ /g, "\\ ");
   let command = `${dir}/bin/seed ${input ? "-i " + input : ""} ${platform ? "-p " + platform : ""} ${output ? "-o " + output : ""} `
-  return exec(command).toString();
+  try {
+    return exec(command).toString();
+  } catch (error) {
+    return error.message;
+  }
 }
 
 exports.export = function (input, modulee, output) {
   const exec = require("child_process").execSync;
   let dir = __dirname.replace(/ /g, "\\ ");
   let command = `${dir}/bin/seed export ${input ? "-i " + input : ""} ${modulee ? "-m " + modulee : ""} ${output ? "-o " + output : ""} `
-  return exec(command).toString();
+  try {
+    return exec(command).toString();
+  } catch (error) {
+    return error.message;
+  }
 }

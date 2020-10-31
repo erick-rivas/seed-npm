8d55b26325c75840bde152bcbf57985e277f89e1
Erick Rivas <erick_rivas@outlook.com>
2020-10-31 05:06:51 -0600

    Update v0.2.31

--- cli.js
+++ cli.js
@@ -3,4 +3,10 @@ const exec = require("child_process").execSync;
 const idx = require("./index.js");
 const [, , ...args] = process.argv
 const command = `${idx.getBin()} ${args.join(" ")}`
-console.log(exec(command).toString())
+
+try{
+  console.log(exec(command).toString())
+}
+catch(error){
+  console.log(error.message)
+}

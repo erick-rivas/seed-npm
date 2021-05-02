5781394c9ba1ad08b7ab8cc0f6c6ca9cc10620c7
Erick Rivas <erick_rivas@outlook.com>
2021-05-01 22:46:00 -0500

    Update v0.3.76

--- cli.js
+++ cli.js
@@ -1,12 +1,17 @@
 #! /usr/bin/env node
-const exec = require("child_process").execSync;
+const exec = require("child_process").exec;
 const idx = require("./index.js");
 const [, , ...args] = process.argv
 const command = `${idx.getBin()} ${args.join(" ")}`
+const child = exec(command)
+process.stdin.pipe(child.stdin)
 
-try{
-  console.log(exec(command).toString())
-}
-catch(error){
-  console.log(error.message)
-}
+child.stdout.on('data', function (data) {
+  process.stdout.write(data.toString());
+});
+child.stderr.on('data', function (data) {
+  console.log(data.toString());
+});
+child.on('close', function (code) {
+  process.exit();
+});
